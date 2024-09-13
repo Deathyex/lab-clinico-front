02235b671c9bb60283cc502de023f108e52e5806
Deathyex <yezithfernando@gmail.com>
2024-09-13 11:08:19 -0500

    Creacion de componentes login, register, lab-services y contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', component: InicioComponent },
+	{ path: 'login', component: LoginComponent },
 ];
---
+++ src/app/components/lab-services/lab-services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-services',
+  standalone: true,
+  imports: [],
+  templateUrl: './lab-services.component.html',
+  styleUrl: './lab-services.component.css'
+})
+export class LabServicesComponent {
+
+}
