efff495de34d6474d60cc52a504a4be247f8f127
Deathyex <yezithfernando@gmail.com>
2024-09-20 13:41:19 -0500

    Pruebas componente profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,20 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LabServicesComponent } from './components/lab-services/lab-services.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', component: InicioComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent },
-	{ path: 'services', component: LabServicesComponent}
+	{ path: 'services', component: LabServicesComponent },
+	{
+		path: 'contact',
+		component: ProfileComponent,
+		children: [
+			{ path: '', pathMatch: 'full', component: LoginComponent },
+			{ path: 'resultados', component: LoginComponent },
+			{ path: 'appointments', component: RegisterComponent },
+		],
+	}, // Ruta para probar componentes
 ];
