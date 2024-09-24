58351e7ab90175683ba10e8742f0387bd84ce067
Santiago Ariza <santiagoari02@outlook.com>
2024-09-24 15:28:10 -0500

    Avances pantalla de servicios

--- src/app/components/lab-services/lab-services.component.ts
+++ src/app/components/lab-services/lab-services.component.ts
@@ -1,28 +1,32 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ExamenesService } from '../../services/examenes.service';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatHint } from '@angular/material/form-field';
 
 @Component({
-  selector: 'app-lab-services',
-  standalone: true,
-  imports: [],
-  templateUrl: './lab-services.component.html',
-  styleUrl: './lab-services.component.css'
+	selector: 'app-lab-services',
+	standalone: true,
+	imports: [MatDatepickerModule, MatHint],
+	templateUrl: './lab-services.component.html',
+	styleUrl: './lab-services.component.css',
 })
-export class LabServicesComponent implements OnInit{
+export class LabServicesComponent implements OnInit {
+	private examenesService = inject(ExamenesService);
+	public examenes: any[] = [];
+	public verModal: boolean = false;
 
-  private examenesService = inject(ExamenesService);
-  public examenes: any[] = [];
+	ngOnInit(): void {
+		this.mostrarExamenes();
+	}
 
-  ngOnInit(): void {
-    this.mostrarExamenes();
-  }
+	private mostrarExamenes() {
+		this.examenesService.getAllExamenes().subscribe(data => {
+			this.examenes = data;
+		});
+	}
 
-  private mostrarExamenes(){
-      this.examenesService.getAllExamenes().subscribe(
-        (data) => {
-          this.examenes = data;
-        }
-      );
-  }
-
+	public crearCita() {
+		this.verModal = !this.verModal;
+		console.log(this.verModal);
+	}
 }
