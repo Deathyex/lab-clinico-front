76487b166080670a9f3d299467f9801a990a1f57
Deathyex <yezithfernando@gmail.com>
2025-01-06 21:55:26 -0500

    Creacion de componentes de administracion

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { AdminExamenesComponent } from './admin-examenes/admin-examenes.component';
+import { AdminResultadosComponent } from './admin-resultados/admin-resultados.component';
+import { AdminUsersComponent } from './admin-users/admin-users.component';
 
 @Component({
-  selector: 'app-admin-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.css'
+	selector: 'app-admin-dashboard',
+	standalone: true,
+	imports: [
+		AdminExamenesComponent,
+		AdminResultadosComponent,
+		AdminUsersComponent,
+	],
+	templateUrl: './admin-dashboard.component.html',
+	styleUrl: './admin-dashboard.component.css',
 })
-export class AdminDashboardComponent {
-
-}
+export class AdminDashboardComponent {}
---
+++ src/app/components/admin-dashboard/admin-examenes/admin-examenes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-examenes',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-examenes.component.html',
+  styleUrl: './admin-examenes.component.css'
+})
+export class AdminExamenesComponent {
+
+}
