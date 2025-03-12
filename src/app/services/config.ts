f984d87ad48cb01124b754ada16d25bbf708ffa8
Deathyex <yezithfernando@gmail.com>
2025-03-12 18:13:41 -0500

    Arreglos en url base, estilos de servicios y comando build

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,6 +1,5 @@
 let env = {
-	//baseUrl: 'http://localhost:3777/api/v1',
-	baseUrl: 'https://lab-clinico-back.onrender.com/api/v1',
+	baseUrl: 'http://localhost:3777/api/v1',
 };
 
 export { env };
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,34 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { env } from './config';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class UserService {
-  private http = inject(HttpClient);
+	private http = inject(HttpClient);
 
-  getAllUsers(): Observable<any[]> {
-    return this.http.get<any[]>('http://localhost:3777/api/v1/users/listAll').pipe(
-      map(users => users.map(user => ({
-        id: user.id,
-        name: user.firstName + ' ' + user.lastName,
-        email: user.email,
-        birthDate: this.calcularEdad(user.birthDate)
-      })))
-    );
-  }
-  
-  calcularEdad(fecha: string) {
-    var hoy = new Date();
-    var cumpleanos = new Date(fecha);
-    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
-    var m = hoy.getMonth() - cumpleanos.getMonth();
+	baseUrl = env.baseUrl;
 
-    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
-        edad--;
-    }
+	getAllUsers(): Observable<any[]> {
+		return this.http.get<any[]>(`${this.baseUrl}/users/listAll`).pipe(
+			map(users =>
+				users.map(user => ({
+					id: user.id,
+					name: user.firstName + ' ' + user.lastName,
+					email: user.email,
+					birthDate: this.calcularEdad(user.birthDate),
+				})),
+			),
+		);
+	}
 
-    return edad;
-  }
+	calcularEdad(fecha: string) {
+		var hoy = new Date();
+		var cumpleanos = new Date(fecha);
+		var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+		var m = hoy.getMonth() - cumpleanos.getMonth();
+
+		if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+			edad--;
+		}
+
+		return edad;
+	}
 }
